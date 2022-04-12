7a826882602147e3b845e36df23c1cd316bbdab7
Jessica Nelson <yahwehschica@yahoo.com>
2022-04-12 16:52:08 -0400

    commit tree 715092babb9164a9ba60f4c9fceaa50456f1a94b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+
+class Anagram 
+
+    attr_accessor :word 
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(str)
+        str.select do |w| 
+            w.split("").sort == word.split("").sort
+        end 
+    end
+end 
+
+#def match(array)
+    #array.select {|x| x.split("").sort == @name.split("").sort}
+  #end 
